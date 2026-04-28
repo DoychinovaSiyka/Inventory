7d5b7280ca14fc32a767647117f554824d572e01
Siyka <doichinova06@abv.bg>
2026-04-29 00:05:56 +0300

    Промените на 28.04.2026

--- graph/warehouse.py
+++ graph/warehouse.py
@@ -3,8 +3,7 @@ class Warehouse:
     def __init__(self, warehouse_id, name, location="Неизвестна"):
         self.warehouse_id = warehouse_id
         self.name = name
-        self.location = location  # Добавям град или адрес за реализъм
-
+        self.location = location
 
     def __repr__(self):
         return f"Warehouse(ID: {self.warehouse_id}, Име: {self.name}, Град: {self.location})"
